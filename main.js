e3525e4aa64281f9504d33d76819d4cdee475b8f
Karina Vazquez <karina.vazquez6@yahoo.com>
2020-11-03 10:16:07 -0600

    final commit

--- main.js
+++ main.js
@@ -1,34 +1,52 @@
 // You should NOT change the HTML or CSS in this project (at least until you reach
 // the bonus objectives). Focus on the JavaScript.
 
-const findInput = document.querySelector(".find-input")
-const replaceInput = document.querySelector(".replace-input")
-const replaceAllButton = document.querySelector(".replace-all-button")
+const findInput = document.querySelector(".find-input");
+const replaceInput = document.querySelector(".replace-input");
+const replaceAllButton = document.querySelector(".replace-all-button");
 
 // The following variable holds your OUTER ARRAY of row elements.
 // Later you will need an OUTER LOOP to loop over the individual elements within
 // this array.
-const rowElements = document.querySelectorAll(".row")
+const rowElements = document.querySelectorAll(".row");
 
 // When you call the function belwo, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
-// NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
-function getCellElements (currentRowElement) {
-    return currentRowElement.querySelectorAll(".cell")
+// NESTED LOOP go? Think through the user's experience: when should WHAT happen?
+function getCellElements(currentRowElement) {
+  return currentRowElement.querySelectorAll(".cell");
 }
 
+replaceAllButton.addEventListener("click", function () {
+  let findText = findInput.value;
+  let replaceText = replaceInput.value;
+  //console.log(findText);
+  //console.log(replaceText);
 
-// YOUR CODE GOES HERE
+  for (let i = 0; i < rowElements.length; i = i + 1) {
+    let cellElements = getCellElements(rowElements[i]);
+    //console.log(cellElements);
 
+    for (let j = 0; j < cellElements.length; j = j + 1) {
+      let cellBox = cellElements[j].innerHTML;
+      console.log(cellBox);
+      if (cellBox.includes(findText)) {
+        console.log(cellBox);
+        cellBox = cellBox.replace(findText, replaceText);
+        console.log(cellBox.replace);
+      }
+    }
+  }
+});
 
-// One last thing: dedicate very careful attention to using variables and
+// YOUR CODE GOES HERE// One last thing: dedicate very careful attention to using variables and
 // naming them accurately.
 // And when you change the value you are assigning to a variable, don't
 // forget to consider changing the name to reflect the change you made! It
 // is very easy to get confused when you are working inside NESTED LOOPS.
-// The best of us do. And unnecessary confusion during the process of 
+// The best of us do. And unnecessary confusion during the process of
 // developing your code means wasted time.
 //
 // The time-cost of structuring and naming things well is FAR less than the
